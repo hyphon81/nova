86cd333d5cc2d948e07c945294572b6a34d8e890
hyphon81 <zero812n@gmail.com>
2016-08-21 18:06:35 +0900

    Workaround for Runtime error

--- nova/scheduler/manager.py
+++ nova/scheduler/manager.py
@@ -52,11 +52,8 @@ class SchedulerManager(manager.Manager):
         if not scheduler_driver:
             scheduler_driver = CONF.scheduler_driver
         
-        self.driver = driver.DriverManager(
-            "nova.scheduler.driver",
-            scheduler_driver,
-            invoke_on_load=True).driver
-        
+        self.driver = importutils.import_object(scheduler_driver)
+    
         super(SchedulerManager, self).__init__(service_name='scheduler',
                                                *args, **kwargs)
 
